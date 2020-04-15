db6ff2d28d3af19fca1aaf593a4625a29c11cca0
Darkweak <sylvain@les-tilleuls.coop>
2020-04-15 21:54:37 +0200

    Improve

---
+++ init-conf.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#########   VARIABLE PARAMETERS SYNTAXS   ############
+#     "domain:folder:service1,service2,service3"     #
+################                      ################
+
+parameters=(
+  "devcv.fr:cv-spa:admin,api,cache,client,mercure,ssr"
+)
+text="[http.routers]"
+
+for x in "${parameters[@]}"
+do
+  IFS=':'
+  read -ra ADDR <<< "$x"
+  domain="${ADDR[0]}"
+  folder="${ADDR[1]}"
+  IFS=','
+  read -ra SERVICES <<< "${ADDR[2]}"
+  for s in "${SERVICES[@]}"
+  do
+    echo "$s"
+    text+="
+  [http.routers.$s-$folder]
+    [http.routers.$s-$folder.tls]
+      [[http.routers.$s-$folder.tls.domains]]
+        main = \"$domain\"
+        sans = [\"*.$domain\"]
+"
+  done
+done
+
+echo "$text" > ./domains.toml
